8be4fd571a5042a12c3f16cdca4a916e9475fd81
Ryo Ikarashi <ryo.5630@gmail.com>
2017-10-31 15:52:26 +0900

    support price volatility

--- coincheck.5s.js
+++ coincheck.5s.js
@@ -2,27 +2,54 @@
 
 const bitbar = require('bitbar');
 const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 const utils = require('./utils');
 const data = utils.requireJSON('./coincheck.json');
 const COINS = require('./coins');
 const ENV = utils.requireJSON('./env.json');
-const API_BASE = 'https://coincheck.com/api'
-const API_RATE_URI = `${API_BASE}/rate`;
-const API_BALANCE_URI = `${API_BASE}/accounts/balance`;
+const API_HOST = 'https://coincheck.com';
+const API_BASE = '/api'
+const API_RATE_URI = `${API_HOST}${API_BASE}/rate`;
+const API_BALANCE_URI = `${API_HOST}${API_BASE}/accounts/balance`;
+const EXCNAHGE_URI = `${API_HOST}/exchange`
 const COMPARATIVE_UNIT = 'jpy';
+
 const getLatestRate = (coin_unit) => axios.get(`${API_RATE_URI}/${coin_unit}_${COMPARATIVE_UNIT}`);
+
 const getAllCoinsRate = Object.entries(COINS).map(coin => ({ [coin[0]]: getLatestRate(coin[1].unit).then(result => result.data.rate) }) );
+
 const allPromises = getAllCoinsRate.map((item, index) => Object.keys(item).map(second => getAllCoinsRate[index][second])[0]);
 
-const getBalance = () => {
-  return axios.get(API_BALANCE_URI, {headers: utils.setSignature(API_BALANCE_URI)});
+const getBalance = () => axios.get(API_BALANCE_URI, {headers: utils.setSignature(API_BALANCE_URI)})
+
+const getHTML = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://coincheck.com/exchange');
+
+  const content = await page.content();
+  const $ = cheerio.load(content);
+  browser.close();
+  return $;
 }
 
-axios.all([...allPromises, getBalance()]).then((result) => {
-  let newData = {};
-  const balance = result[result.length - 1].data;
+const fixPrefix = (volatility) => ~volatility.indexOf('-') ? volatility.replace('+', '') : volatility;
+
+const getVolatility = ($) => {
+  const $coins = $('.currency_list td');
+  let volatilities = {};
+  $coins.each((i, element) => {
+    const name = $(element).find('.currency_name').text().toLowerCase().replace(' ', '_');
+    volatilities[name] = fixPrefix($(element).find('.currency_desc.up').text());
+  });
+  return volatilities;
+}
+
+axios.all([...allPromises, getBalance(), getHTML()]).then((result) => {
+  const balance = result[result.length - 2].data;
+  const $ = result[result.length - 1];
+  const volatilities = getVolatility($);
   let total = Number(balance.jpy);
 
   const bitbarContent = getAllCoinsRate.map((coin, index) => {
@@ -31,26 +58,19 @@ axios.all([...allPromises, getBalance()]).then((result) => {
     const unit  = entries[index][1].unit;
     const name  = entries[index][0];
     const rate = result[index];
-    const difference = data[name] - rate;
-    const prefix = difference >= 0 ? '-' : '+';
+    const volatility = volatilities[name];
 
     total += balance[unit] * rate;
 
-    newData[name] = rate;
-
     return {
-      text: `${rate} (${prefix}${Math.abs(utils.floatFormat(difference, 3))}) [${unit.toUpperCase()}]`,
+      text: `${rate} (${volatility}) [${unit.toUpperCase()}]`,
       templateImage: image,
-      color: prefix === '-' ? ENV.colors.red : ENV.colors.green,
+      color: volatility.substr(0,1) === '-' ? ENV.colors.red : ENV.colors.green,
       href: `https://coincheck.com/ja/exchange/charts/coincheck/${unit}_jpy/300`
-    }
+    };
   });
 
-  fs.writeFile(path.resolve(__dirname, 'coincheck.json'), JSON.stringify(newData), () => {});
-
-  const totalBalance = {
-    text: `¥${utils.floatFormat(total, 3)}`,
-  };
+  const totalBalance = { text: `¥${utils.floatFormat(total, 3)}` };
 
   bitbar([
     totalBalance,
