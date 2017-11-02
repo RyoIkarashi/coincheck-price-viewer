1d955efe398f70f2465f90c875900d53309754e3
Ryo Ikarashi <ryo.5630@gmail.com>
2017-11-03 00:36:10 +0900

    add metadata

--- coincheck.5s.js
+++ coincheck.5s.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env /usr/local/bin/node
 
+# <bitbar.title>Coincheck Price Viewer</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Ryo Ikarashi</bitbar.author>
+# <bitbar.author.github>RyoIkarashi</bitbar.author.github>
+# <bitbar.desc>Display the spot JPY prices of cryptocurrencies and your current balance in coincheck.com</bitbar.desc>
+# <bitbar.image>https://user-images.githubusercontent.com/5750408/32211052-25739bd4-be54-11e7-8d99-e65b1fc00d41.png</bitbar.image>
+# <bitbar.dependencies>node</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/RyoIkarashi/coincheck-price-viewer</bitbar.abouturl>
+
+# If you feel this little tool gives you some value, tips are always welcome at the following addresses!
+# Bitcoin: 1DrLPjzmNHtkdBstd82xvCxGY38PnKauRH
+# Mona:    MC7XMmi1YXoJH19D1q4H8ijBkdvarWBTMi
+
 const bitbar = require('bitbar');
 const axios = require('axios');
 const puppeteer = require('puppeteer');
